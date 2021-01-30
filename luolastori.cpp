7f2ad879551dda873c8d13de1690cbbe72b4eff3
Pekka Korolainen <pekka.korolainen@gmail.com>
2021-01-30 13:11:20 +0200

    first steps

--- luolastori.cpp
+++ luolastori.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+int luola[10][10];
 
 int main() {
-	std::cout << "moro" << std::endl;
+	luola[2][2] = 1;
+	luola[5][8] = 1;
+	for (int i = 0; i<10; i++) {
+		for (int j = 0; j<10; j++) {
+			if (luola[i][j] == 0)
+				std::cout << "X";
+			else
+				std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
 }
