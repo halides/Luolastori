fa1e9ca5964b6b589f7d1eceb9bd4f6cb41b8d7e
Pekka Korolainen <pekka.korolainen@gmail.com>
2021-02-18 13:45:08 +0200

    kommentointi koodiin

--- headers/luolastori.hpp
+++ headers/luolastori.hpp
@@ -6,29 +6,29 @@
 class Luola
 {
 	private:
-		int luola[LUOLASTON_KOKO][LUOLASTON_KOKO];
-		int suunnat[4];
-		int monesko;
+		int luola[LUOLASTON_KOKO][LUOLASTON_KOKO];	//vuoltava luolasto array-of-array
+		int suunnat[4];	//array suunnille joihin käytäväalgo voi mennä
+		int monesko;	//id luotavalle alueelle
 
-		void tee_sokkelo(int x, int y);
-		int* sotke();
-		void fillaa(int x, int y);
-		int tila(int x, int y);
-		void vuole(int x, int y);
-		void tayta(int x, int y);
+		void tee_sokkelo(int x, int y);	//lähtee luomaan sokkeloa kohdasta (x,y) jos siinä on seinää
+		int* sotke();			//tämä  kuljettavan suunnan käytäväalgolle
+		void fillaa(int x, int y);	//flood fill
+		int tila(int x, int y);		//palauttaa onko ko. kohdassa seinää (0) tai alueen id:n (>0) johon kohta kuuluu
+		void vuole(int x, int y);	//tekee ko. kohdasta aluetta "monseko" muuttujan mukaan
+		void tayta(int x, int y);	//tekee ko. kohdasta seinää (käytetään ainoastaan konstruktorissa
 
-		//for tests
+		//testejä varten
 		int testi_laskuri;
 		void fillaa_testille(int x, int y, int *laskuri);
 	public:
 		Luola();
-		void tee_huone(bool);
-		void tee_sokkelot();
-		void puhko();
-		void tulosta();
+		void tee_huone(bool);	//luo huoneen, bool määrittelee stäkkääkö huoneita päällekkäin vai ei
+		void tee_sokkelot();	//kutsuu tee_sokkeloa jokaiselle parittomalle (x,y)-parille
+		void puhko();		//yhdistää alueet toisiinsa
+		void tulosta();		//tulostaa sokkelon
 		void poista_umpikujat();
 
-		//for tests
+		//testejä varten
 		int montako_tyhjaa();
 		int moneenko_fillaa();
 };
--- luolastori.cpp
+++ luolastori.cpp
@@ -4,13 +4,14 @@
 #include <iostream>
 
 Luola::Luola() {
-	srand(time(NULL));
-	suunnat[0] = 0;
+	srand(time(NULL));	//alusta randomizeri kellosta (sekunnin tarkkuus)
+	suunnat[0] = 0;		//alusta ilmansuunnat
 	suunnat[1] = 1;
 	suunnat[2] = 2;
 	suunnat[3] = 3;
-	monesko = 1;
+	monesko = 1;		//alueitten id-laskuri
 
+	//alusta luolasto pelkäksi seinäksi
         for (int i = 0; i < LUOLASTON_KOKO; i++) {
                 for (int j = 0; j < LUOLASTON_KOKO; j++) {
                         tayta(i,j);
@@ -30,28 +31,34 @@ void Luola::tulosta() {
 }
 
 
+//>0 on jotain muuta kuin seinää. monekso on alueen id, id:tä käytetään alueiden yhdistämisessä
 void Luola::vuole(int x, int y) {
 	luola[x][y] = monesko;
 }
 
 
+//0 on seinää
 void Luola::tayta(int x, int y) {
 	luola[x][y] = 0;
 }
 
 
+//palauttaa joko 0 jos seinää tai alueen id:n
 int Luola::tila(int x, int y) {
 	return luola[x][y];
 }
 
 
 void Luola::tee_huone(bool pinoa) {
+	//luodaan mitat huoneelle, jos menisi yli luolaston niin return
 	int x = ((rand() % ((LUOLASTON_KOKO-HUONEEN_KOKO-1)/2)) * 2 + 1);
 	int y = ((rand() % ((LUOLASTON_KOKO-HUONEEN_KOKO-1)/2)) * 2 + 1);
 	int sivu = rand() % HUONEEN_KOKO * 2 + 3;
 	int sivu2 = rand() % HUONEEN_KOKO * 2 + 3;
 	if (x+sivu > LUOLASTON_KOKO-1) return;
 	if (y+sivu2 > LUOLASTON_KOKO-1) return;
+
+	//tarkastaa menisikö uusi huone jo luodun alueen päälle, jos menee niin return
 	if (pinoa == false) {
 		int xx = x;
 		int yy = y;
@@ -72,15 +79,19 @@ void Luola::tee_huone(bool pinoa) {
 		}
 	}
 //	std::cout << "    teen: " << x << " " << y << " " << sivu << " " << sivu2 << " " << std::endl;
+//
+	//tämä tekee itse huoneen
 	for (int i = x; i<x+sivu; i++) {
 		for (int j = y; j<y+sivu2; j++) {
 			vuole(i,j);
 		}
 	}
+	//lisätään alue-id-laskuria
 	monesko++;
 }
 
 
+//lähde tekemään sokkeloa jokaisesta parittomasta (x,y) parista. lisätään alue-id-laskuria jos luotiin alue
 void Luola::tee_sokkelot() {
 	for (int i = 1; i<LUOLASTON_KOKO-1; i+=2) {
 		for (int j = 1; j<LUOLASTON_KOKO-1; j+=2) {
@@ -93,11 +104,13 @@ void Luola::tee_sokkelot() {
 }
 
 
+//käydään luolasto läpi, ja jos löytyy jokin kohta joka ei ole seinää mutta jonka ympärillä
+//on vähintään 3 seinää, niin täytetään ko. alue. tämä poistaa umpikujat.
+//i-- ja j-- siirtyy kolumnin ja rivin taaksepäin, jotta myös vasemmalta ja ylhäältä
+//tulevat umpikujat poistuvat
 void Luola::poista_umpikujat() {
 	for (int i = 1; i<LUOLASTON_KOKO-1; i+=1) {
 		for (int j = 1; j<LUOLASTON_KOKO-1; j+=1) {
-//			if (i == 1 && j == 1) continue;
-//			if (i == LUOLASTON_KOKO-1 && j == LUOLASTON_KOKO-1) continue;
 			int laskuri = 0;
 			if (tila(i,j) > 0) {
 				if(tila(i+1,j) == 0) laskuri++;
@@ -115,6 +128,8 @@ void Luola::poista_umpikujat() {
 }
 
 
+//jos löytyy seinä jonka joko horisontaalisesti tai vertikaalisti on kahta eri aluetta,
+//poistetaan ko. seinä ja ajetaan flood fill joka päivittää näiden kahden eri alueen id:t samaksi
 void Luola::puhko() {
 	for (int i = 1; i<LUOLASTON_KOKO-1; i+=1) {
 		for (int j = 1; j<LUOLASTON_KOKO-1; j+=1) {
@@ -133,6 +148,7 @@ void Luola::puhko() {
 }
 
 
+//simppeli rekursiivinen flood fill
 void Luola::fillaa(int x, int y) {
 	vuole(x,y);
 	if (tila(x+1,y) > 0 && tila(x+1,y) != monesko) fillaa(x+1,y);
@@ -142,6 +158,7 @@ void Luola::fillaa(int x, int y) {
 }
 
 
+//sama kuin yllä mutta laskurilla joka laskee montako neliötä filli löysi
 void Luola::fillaa_testille(int x, int y, int *laskuri) {
 	vuole(x,y);
 	if (tila(x+1,y) > 0 && tila(x+1,y) != monesko) {(*laskuri)++; fillaa_testille(x+1,y,laskuri);}
@@ -151,6 +168,8 @@ void Luola::fillaa_testille(int x, int y, int *laskuri) {
 }
 
 
+//heitetään kolikkoa kolme kertaa, jos kruuna niin swapataan suunnat[]-arrayssa olevia arvoja ympäriinsä
+//tämä on riittävä satunnaisuus tässä kontekstissa
 int* Luola::sotke() {
 	int vali;
 	if (rand()%2) {
@@ -168,41 +187,47 @@ int* Luola::sotke() {
 		suunnat[0] = suunnat[1];
 		suunnat[1] = vali;
 	}
-	return suunnat;
 //	std::cout << suunnat[0] << suunnat[1] << suunnat[2] << suunnat[3] << std::endl;
+	return suunnat;
 }
 
+
+//rekursiivinen luolaston generointi.
+//tarvitaan mun_suunnat[] johon kopioidaan lokaali suuntajärjestys.
+//jos vain käytetään globaalia suunnat[]-arraya, rekursio kutsuu sotke() algoritmia
+//joka sekoittaa globaalin suunnat[]-arrayn jolloin lokaalisti ei välttämättä lähdetä joka suuntaan.
+//tämä on sinänsä ihan ok mutta tämä luo yhtenäisemmän luolaston.
 void Luola::tee_sokkelo(int x, int y) {
 	vuole(x,y);
-	int* heh = sotke();
+	int* g_suunnat = sotke();
 	int mun_suunnat[4];
-	mun_suunnat[0] = heh[0];
-	mun_suunnat[1] = heh[1];
-	mun_suunnat[2] = heh[2];
-	mun_suunnat[3] = heh[3];
+	mun_suunnat[0] = g_suunnat[0];
+	mun_suunnat[1] = g_suunnat[1];
+	mun_suunnat[2] = g_suunnat[2];
+	mun_suunnat[3] = g_suunnat[3];
 
 	for (int i=0; i<4; i++) {
 		int suunta = mun_suunnat[i];
 		switch(suunta) {
-		case 0:
+		case 0: //"oikealle"
 			if ((x+2<LUOLASTON_KOKO) && tila(x+2,y) == 0) {
 				vuole(x+1,y);
 				tee_sokkelo(x+2,y);
 			}
 		break;
-		case 1:
+		case 1: //"vasemmalle"
 			if ((x-2>0) && tila(x-2,y) == 0) {
 				vuole(x-1,y);
 				tee_sokkelo(x-2,y);
 			}
 		break;
-		case 2:
+		case 2: //"alas"
 			if ((y+2<LUOLASTON_KOKO) && tila(x,y+2) == 0) {
 				vuole(x,y+1);
 				tee_sokkelo(x,y+2);
 			}
 		break;
-		case 3:
+		case 3: //"ylös"
 			if ((y-2>0) && tila(x,y-2) == 0) {
 				vuole(x,y-1);
 				tee_sokkelo(x,y-2);
@@ -213,6 +238,9 @@ void Luola::tee_sokkelo(int x, int y) {
 }
 
 
+
+//TESTEJÄ VARTEN
+//tämä laskee montako tyhjää (eli ei-seinää, eli >0) luolastossa on
 int Luola::montako_tyhjaa() {
 	int laskuri = 0;
 	for(int i=0;i < LUOLASTON_KOKO; i++) {
@@ -224,6 +252,8 @@ int Luola::montako_tyhjaa() {
 }
 
 
+//TESTEJÄ VARTEN
+//tämä etsii jonkin kohdan luolastosta joka ei ole seinää ja ajaa siihen flood fillin
 int Luola::moneenko_fillaa() {
 	testi_laskuri = 0;
 	monesko++;
