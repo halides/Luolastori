0237e8e855f7d16f407e3fd355c114fdd381050f
Pekka Korolainen <pekka.korolainen@gmail.com>
2021-03-02 16:19:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
-		std::cout << "./luolastori <int>" << std::endl;
+		std::cout << "./luolastori <int> #jos int on 0, tulostetaan satunnainen sokkelo" << std::endl;
 		return 1;
 	}
 	Luola l(atoi(argv[1]));
